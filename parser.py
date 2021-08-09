f85d9991dc59eaf453723d12cc74fd950b726775
Yash Jaiswal <yashjaiswal1@outlook.com>
2021-08-09 21:04:27 +0530

    Add function to parse XML tree

--- parser.py
+++ parser.py
@@ -9,4 +9,29 @@ def fetchRSS():
         file.write(response.content)
 
 
+def parseXML(filename):
+    tree = ET.parse(filename)
+    root = tree.getroot()
+
+    # array of dictionaries consisting of data present in <Item>
+    blogitems = []
+
+    for item in root.findall("./channel/item"):
+
+        blog = {}
+
+        for child in item:
+            if child.tag == "description" or child.tag == "guid":
+                pass
+            elif child.tag == "{http://purl.org/dc/elements/1.1/}creator":
+                blog["creator"] = child.text
+            else:
+                blog[child.tag] = child.text
+
+        blogitems.append(blog)
+
+    return blogitems
+
+
 fetchRSS()
+parseXML("fetchedBlogs.xml")
