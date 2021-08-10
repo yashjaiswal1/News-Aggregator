b9bab6944c29c90ee67ed2ecbc8818e378acf546
Yash Jaiswal <yashjaiswal1@outlook.com>
2021-08-10 22:10:19 +0530

    Add API route to fetch all blogs

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from collections import defaultdict
+import sqlite3
+import json
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,13 +13,23 @@ class HelloWorld(Resource):
         return {"testJSON": "Hello World!"}
 
 
-class Multi(Resource):
-    def get(self, num):
-        return {"result": num*10}
+class BlogList(Resource):
+    def get(self):
+        connection = sqlite3.connect(
+            "/Users/yashjaiswal/Desktop/intern_project/News-Aggregator/RSSFeed.db")
+        cur = connection.cursor()
+
+        query = "SELECT * FROM Blogs ORDER BY(PubDate) DESC;"
+        cur.execute(query)
+
+        rows = cur.fetchall()
+        connection.commit()
+        connection.close()
+        return rows
 
 
 api.add_resource(HelloWorld, "/")
-api.add_resource(Multi, "/multi/<int:num>")
+api.add_resource(BlogList, "/api")
 
 if __name__ == "__main__":
     app.run(debug=True)
