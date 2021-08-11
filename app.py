c1f66dba1184c1187584c246e4c755b78799ec9d
Yash Jaiswal <yashjaiswal1@outlook.com>
2021-08-11 13:12:33 +0530

    Add query params for DateRange and Author

--- app.py
+++ app.py
@@ -19,13 +19,27 @@ class BlogList(Resource):
             "/Users/yashjaiswal/Desktop/intern_project/News-Aggregator/RSSFeed.db")
         cur = connection.cursor()
 
-        page = int(request.args.get("page")) if request.args.get("page") else 1
+        # fetching string query params
+        page = int(request.args.get("page")) if (request.args.get(
+            "page") != None and int(request.args.get("page")) >= 1) else 1
         limit = int(request.args.get("limit")
-                    ) if request.args.get("limit") else 50
+                    ) if (request.args.get("limit") != None and int(request.args.get("limit")) >= 0) else 50
+        author = request.args.get("author")
+        start_datetime = request.args.get("start") if request.args.get(
+            "start") else "2021-01-01 00:00:00"
+        end_datetime = request.args.get("end") if request.args.get(
+            "end") else "2022-01-01 00:00:00"
 
-        query = f"SELECT * FROM Blogs ORDER BY(PubDate) DESC LIMIT {(page-1) * limit},{limit};"
-        cur.execute(query)
+        query_start = f"SELECT * FROM Blogs WHERE PubDate BETWEEN '{start_datetime}' AND '{end_datetime}' "
+        query_mid = f'AND Author="{author}" '
+        query_end = f"ORDER BY(PubDate) DESC LIMIT {(page-1) * limit},{limit};"
 
+        if author:
+            query_main = query_start + query_mid + query_end
+        else:
+            query_main = query_start + query_end
+
+        cur.execute(query_main)
         rows = cur.fetchall()
         connection.commit()
         connection.close()
--- parser.py
+++ parser.py
@@ -13,23 +13,36 @@ def fetchRSS():
 def parseXML(filename):
     tree = ET.parse(filename)
     root = tree.getroot()
+    months = {"Jan": "01", "Feb": "02", "Mar": "03", "Apr": "04", "May": "05", "Jun": "06",
+              "Jul": "07", "Aug": "08", "Sep": "09", "Oct": "10", "Nov": "11", "Dec": "12"}
 
-    # array of dictionaries consisting of data present in <Item>
+    # list of dictionaries consisting of data present in <Item>
     blogitems = []
 
+    # traversing XML tree to fetch data
     for item in root.findall("./channel/item"):
-
         blog = {}
 
         for child in item:
             if child.tag == "description" or child.tag == "guid":
                 pass
+
             if child.tag == "pubDate":
-                blog[child.tag] = child.text[5:25]
+                pubDate = child.text[5:25]
+                day = pubDate[:2]
+                month = months[pubDate[3:6]]
+                year = pubDate[7:11]
+                time = pubDate[12:]
+                # datetime format = "YYYY-MM-DD HH:MM:SS"
+                formatted_datetime = year + "-" + month + "-" + day + " " + time
+                blog[child.tag] = formatted_datetime
+
             elif child.tag == "{http://purl.org/dc/elements/1.1/}creator":
                 blog["author"] = child.text
+
             else:
                 blog[child.tag] = child.text
+
         blogitems.append(blog)
 
     return blogitems
@@ -56,9 +69,11 @@ def saveToDB(blogitems):
     connection = sqlite3.connect(
         "/Users/yashjaiswal/Desktop/intern_project/News-Aggregator/RSSFeed.db")
     cur = connection.cursor()
+
     for item in blogitems:
         query = f'INSERT INTO Blogs VALUES("{item["title"]}", "{item["pubDate"]}", "{item["link"]}", "{item["author"]}", "{item["comments"]}");'
         cur.execute(query)
+
     connection.commit()
     connection.close()
 
