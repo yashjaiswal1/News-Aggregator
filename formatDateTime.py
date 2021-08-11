3479ee542e10b45dfd5e48f3730cf05c6d5eabdd
Yash Jaiswal <yashjaiswal1@outlook.com>
2021-08-11 13:10:26 +0530

    Add script to format DateTime string to SQLite format

---
+++ formatDateTime.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+
+def formatDateTime():
+    months = {"Jan": "01", "Feb": "02", "Mar": "03", "Apr": "04", "May": "05", "Jun": "06",
+              "Jul": "07", "Aug": "08", "Sep": "09", "Oct": "10", "Nov": "11", "Dec": "12"}
+    params = []
+    connection = sqlite3.connect(
+        "/Users/yashjaiswal/Desktop/intern_project/News-Aggregator/RSSFeed.db")
+    cur = connection.cursor()
+    cursor = cur.execute("SELECT * FROM Blogs;")
+
+    for row in cursor:
+        pubDate = row[1]
+        day = pubDate[:2]
+        month = months[pubDate[3:6]]
+        year = pubDate[7:11]
+        time = pubDate[12:]
+        formatted_datetime = year + "-" + month + "-" + day + " " + time
+        params.append((formatted_datetime, row[4]))
+    print(params)
+
+    query = """UPDATE Blogs SET PubDate = ? WHERE CommentsURL = ?"""
+    cur.executemany(query, params)
+    connection.commit()
+    connection.close()
+
+
+formatDateTime()
